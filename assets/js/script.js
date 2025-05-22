fde65178a83a70435df51e08ed4c97900bc20fea
šeki <seekii.official@gmail.com>
2025-05-22 18:55:02 +0200

    about me added ^-^

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,42 @@
 var cTag = null
-function go_to(tag,force=true) {
-  if (cTag == tag){return}
-  cTag = tag
-  if(force){
-    $('html, body').stop().animate({scrollTop: $("#"+tag).offset().top}, 500);
-  }
-  $("nav .active").removeClass("active");
-  $(`a[href='/` + "#"+tag + `']`).parent().addClass("active");
-  console.log(cTag + " - "+tag)
+
+function go_to(tag, force = true) {
+    if (cTag == tag) {
+        return
+    }
+    cTag = tag
+    if (force) {
+        $('html, body').stop().animate({
+            scrollTop: $("#" + tag).offset().top
+        }, 500);
+    }
+    $("nav .active").removeClass("active");
+    $(`a[href='/` + "#" + tag + `']`).parent().addClass("active");
+    console.log(cTag + " - " + tag)
 }
 
-$("a").on("click",function(e){
-  var href = $(this).attr("href")
-  if (href.split("#").length == 2){
-    e.preventDefault()
-  }
-  go_to(href.split("#")[1])
+$("a").on("click", function(e) {
+    var href = $(this).attr("href")
+    if (href.split("#").length == 2) {
+        e.preventDefault()
+    }
+    go_to(href.split("#")[1])
 })
-if (window.location.href.split("#")[1]){
-  var tag = window.location.href.split("#")[1]
-  go_to(tag)
+if (window.location.href.split("#")[1]) {
+    var tag = window.location.href.split("#")[1]
+    go_to(tag)
+} else {
+    go_to("home")
 }
-else{go_to("home")}
 var end = false
 
 $(document).ready(function() {
+    $("#home p").text(`${check_age()} year old who sometimes programs in his free time`)
     $("#contact_form").submit(function(event) {
         event.preventDefault();
-        if (end){return}
+        if (end) {
+            return
+        }
         $("button[type='submit']").text("Sending...")
         end = true
         var formData = $(this).serialize();
@@ -48,7 +57,7 @@ $(document).ready(function() {
                 content: `**Ime:** ${$("#name").val()}\n**Email:** ${$("#email").val()}\n**Poruka:** ${$("#message").val()}`
             }),
             success: function(response) {
-                $("form input, form textarea").attr("disabled",true)
+                $("form input, form textarea").attr("disabled", true)
                 $("button[type='submit']").text("Message sent! ^-^")
                 $("button[type='submit']").addClass("act")
                 $("#contact_form").trigger("reset");
@@ -62,11 +71,12 @@ $(document).ready(function() {
 $(window).scroll(function() {
     var scrollPosition = $(this).scrollTop();
     var sections = [
-        { id: "home", element: $("#home") },
-        { id: "projects", element: $("#projects") },
-        { id: "skills", element: $("#skills") },
-        { id: "feedback", element: $("#feedback") },
-        { id: "contact", element: $("#contact") }
+        {id: "home",element: $("#home")},
+        {id: "projects",element: $("#projects")},
+        {id: "skills",element: $("#skills")},
+        {id: "about",element: $("#about")},
+        {id: "feedback",element: $("#feedback")},
+        {id: "contact",element: $("#contact")}
     ];
 
     for (var i = sections.length - 1; i >= 0; i--) {
@@ -78,24 +88,22 @@ $(window).scroll(function() {
 });
 
 var opened = null
-$(".bi-image").on("click",function(){
+$(".bi-image").on("click", function() {
     $(".preview").slideUp(300)
     var parent = $(this).parent().parent().parent()
     var img = parent.find("img")
-    if (img.attr("src") == opened){return}
+    if (img.attr("src") == opened) {
+        return
+    }
     opened = img.attr("src")
     console.log(parent)
     img.slideDown(300)
 })
 
-function check_age(){
+function check_age() {
     var born = 2002;
     var month = 9;
     var age = new Date().getFullYear() - born;
     if (new Date().getMonth() + 1 < month) age--;
     return age;
 }
-
-$(document).ready(function(){
-    $("#home p").text(`${check_age()} year old who sometimes programs in his free time`)
-})
